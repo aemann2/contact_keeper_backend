98b760d7a5a7b0c012e5f50a377c35f52920ea8b
aemann2 <aemann2@gmail.com>
2022-09-10 18:12:42 -0700

    added AUTHENTICATION_BACKENDS

--- src/settings.py
+++ src/settings.py
@@ -34,6 +34,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
 
 # Application definition
 
