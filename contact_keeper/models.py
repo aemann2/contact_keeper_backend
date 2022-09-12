e4220a448540ff5403d59ccc011ea04535d85b4f
aemann2 <aemann2@gmail.com>
2022-09-12 16:41:13 -0700

    fixed auth problem for prod

--- contact_keeper/models.py
+++ contact_keeper/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from phone_field import PhoneField
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=100)
+
+
+class CustomPhoneField(PhoneField):
+    # overwriting validation error for PhoneField
+    @staticmethod
+    def _validate_E164(value):
+        if value and not value.is_E164:
+            raise ValidationError("E164 error: Please enter a valid phone number")
+
+
+class Contact(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = CustomPhoneField(E164_only=True, max_length=15)
+    PERSONAL = "Personal"
+    PROFESSIONAL = "Professional"
+    CONTACT_TYPE_CHOICES = [
+        (PERSONAL, "Personal"),
+        (PROFESSIONAL, "Professional"),
+    ]
+    contact_type = models.CharField(
+        max_length=12,
+        choices=CONTACT_TYPE_CHOICES,
+        default=PROFESSIONAL,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 import os
 from dotenv import load_dotenv
-
 import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,14 +25,14 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8%7*g@=q)jlsvbx4pyn2zkb#b-^n#ah@gi0@z8f**+&3ix&)@m"
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
 
 # Application definition
 
@@ -44,7 +43,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "contact_keeper",
+    "phone_field",
 ]
+
+AUTH_USER_MODEL = "contact_keeper.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -121,19 +124,5 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-NAME = os.getenv("NAME")
-USER = os.getenv("USER")
-PASSWORD = os.getenv("PASSWORD")
-HOST = os.getenv("HOST")
-PORT = os.getenv("PORT")
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": NAME,
-        "USER": USER,
-        "PASSWORD": PASSWORD,
-        "HOST": HOST,
-        "PORT": PORT,
-    }
-}
+DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
