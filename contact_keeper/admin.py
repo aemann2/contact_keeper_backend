2ed2490e149359b161f05c4615b65d6016db9be7
aemann2 <aemann2@gmail.com>
2022-09-15 22:14:47 -0700

    admin config

--- contact_keeper/admin.py
+++ contact_keeper/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import User, Contact
 
 # Register your models here.
+admin.site.register(User)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+    fields = (
+        "owner",
+        "name",
+        "email",
+        "phone",
+        "contact_type",
+        "created_at",
+        "updated_at",
+    )
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "phone",
+        "contact_type",
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
